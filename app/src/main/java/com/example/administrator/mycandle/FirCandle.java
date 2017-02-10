ca62ec0d046f51e2f12898ac8b846193841595d0
Sunhulg <1048054782@qq.com>
2017-02-10 10:24:46 +0800

    Rejected commit: Default

---
+++ app/src/main/java/com/example/administrator/mycandle/FirCandle.java
@@ -0,0 +1,12 @@
+package com.example.administrator.mycandle;
+
+/**
+ * Created by Administrator on 2016/12/27 0027.
+ */
+
+public class FirCandle extends ICandle {
+
+    public FirCandle(int x, int y) {
+        super(x, y);
+    }
+}
